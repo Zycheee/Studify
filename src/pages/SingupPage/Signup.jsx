78387916ee01b80587c4b23b5632c56bd875b1d3
Zyche <christiankyleducay321@gmail.com>
2025-10-11 00:59:54 +0800

    bruh

--- src/pages/SingupPage/Signup.jsx
+++ src/pages/SingupPage/Signup.jsx
@@ -153,7 +153,7 @@ const Signup = () => {
                 />
                 {error.password && (
                   <p className="text-red-500 text-sm ">
-                    Password must be more than 6 characters.
+                    Please enter a valid email address.
                   </p>
                 )}
                 <input
@@ -167,7 +167,7 @@ const Signup = () => {
                 />
                 {error.password && (
                   <p className="text-red-500 text-sm ">
-                    Please enter a valid email address.
+                    Password must be more than 6 characters.
                   </p>
                 )}
                 <input
