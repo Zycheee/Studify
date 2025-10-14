87b18f087cfd0361dc288fa57addbcb0d976dfd4
Zyche <christiankyleducay321@gmail.com>
2025-10-14 22:40:25 +0800

    minor changes

--- src/pages/components/Streak.jsx
+++ src/pages/components/Streak.jsx
@@ -2,23 +2,15 @@ import React from "react";
 import FireStreakIcon from "/fire-joypixels.gif";
 
 const Streak = () => {
-  const streak = [{ name: "Haggai Estavilla", streakDays: 69 }];
+  const person = { name: "Haggai Estavilla", streakDays: 69 };
 
   return (
-    <div className="flex justify-center p-5 w-full mt-2 font-sans hover:scale-108 t transition-all ease-in-out delay-100">
-      {streak.map((person, index) => (
-        <div key={index} className="flex flex-col items-center gap-3">
-          <img
-            src={FireStreakIcon}
-            alt="Fire Streak"
-            className="flexh-25 w-25 "
-          />
-          <span className="font-semibold text-6xl pl-3">
-            {person.streakDays}
-          </span>
-          <span className="font-semibold text-2xl pl-5 ">Day streak</span>
-        </div>
-      ))}
+    <div className="flex justify-center p-5 w-full mt-2 font-sans hover:scale-105 transition-all ease-in-out duration-200">
+      <div className="flex flex-col items-center gap-3">
+        <img src={FireStreakIcon} alt="Fire Streak" className="h-25 w-25" />
+        <span className="font-semibold text-6xl pl-3">{person.streakDays}</span>
+        <span className="font-semibold text-2xl pl-5 ">Day streak</span>
+      </div>
     </div>
   );
 };
